f03c9763648f70da4ee9f5abe3d1b838ba780c04
Vitor Costansi do Nascimento <vi_vcn@hotmail.com>
2024-10-12 15:22:34 -0300

    feat: new challenger desafio-escrendo-classes-jogo.js

---
+++ desafio-escrevendo-classes-jogo.js
@@ -0,0 +1,24 @@
+
+class heroi {
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar(){
+        if(this.tipo == "guerreiro"){
+            console.log(`O ${this.tipo} atacou usando espada!`)
+        }else if(this.tipo == "mago"){
+            console.log(`O ${this.tipo} atacou usando magia!`)
+        }else if(this.tipo == "monge"){
+            console.log(`O ${this.tipo} atacou usando artes marciais!`)
+        }else if(this.tipo == "ninja"){
+            console.log(`O ${this.tipo} atacou usando shuriken!`)
+        }
+    }
+}
+
+let personagem = new heroi("Shiu", 50, "ninja")
+
+personagem.atacar()
